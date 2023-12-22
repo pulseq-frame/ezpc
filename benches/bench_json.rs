83c5434432544f84e30509a7e0f4c733b1d1d9eb
J-Endres <67545816+J-Endres@users.noreply.github.com>
2023-12-22 11:31:46 +0100

    Added benchmark comparison to pom

--- benches/bench_json.rs
+++ benches/bench_json.rs
@@ -1,14 +1,26 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
+mod json_pom;
 mod json_text_parse;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
+fn json(c: &mut Criterion) {
     let input = std::fs::read_to_string("assets/data.json").unwrap();
+    let mut group = c.benchmark_group("json");
 
-    c.bench_function("parse json with text-parse", |b| {
-        b.iter(|| json_text_parse::json().parse(black_box(&input)))
+    group.bench_function("pom", |b| {
+        b.iter(|| {
+            json_pom::json().parse(black_box(input.as_bytes())).ok();
+        })
     });
+
+    group.bench_function("text-parse", |b| {
+        b.iter(|| {
+            json_text_parse::json().parse(black_box(&input)).ok();
+        })
+    });
+
+    group.finish();
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, json);
 criterion_main!(benches);
